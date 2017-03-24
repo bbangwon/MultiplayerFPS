3c62de867b15c83e8c3b9c6e32bab327e757a254
bbangwon <bbangwon.chung@gmail.com>
2017-03-24 10:59:30 +0900

    6장까지 완료

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -6,24 +6,49 @@ public class PlayerSetup : NetworkBehaviour {
     [SerializeField]
     Behaviour[] componentsToDisable;
 
+    [SerializeField]
+    string remoteLayerName = "RemotePlayer";
+
     Camera sceneCamera;
 
     private void Start()
     {
+        //Disable components tha should only be
+        //active on the player that we control
         if(!isLocalPlayer)
         {
-            for (int i = 0; i < componentsToDisable.Length; i++)
-            {
-                componentsToDisable[i].enabled = false;
-            }
+            DisableComponents();
+            AssignRemoveLayer();
         }
         else
         {
+            //We are the local player: Disable the scene camera
             sceneCamera = Camera.main;
             if (sceneCamera != null)
             {
                 sceneCamera.gameObject.SetActive(false);
             }
+        }
+
+        RegisterPlayer();
+    }
+
+    void RegisterPlayer()
+    {
+        string _ID = "Player " + GetComponent<NetworkIdentity>().netId;
+        transform.name = _ID;
+    }
+
+    void AssignRemoveLayer()
+    {
+        gameObject.layer = LayerMask.NameToLayer(remoteLayerName);
+    }
+
+    void DisableComponents ()
+    {
+        for (int i = 0; i < componentsToDisable.Length; i++)
+        {
+            componentsToDisable[i].enabled = false;
         }
     }
 
