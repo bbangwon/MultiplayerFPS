15fc4097a96d4464b658d60b3d015c85128fe7d8
정영일 <bbangwon.chung@gmail.com>
2017-03-31 17:13:32 +0900

    12장까지 완료

---
+++ Assets/WeaponManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class WeaponManager : NetworkBehaviour {
+
+    [SerializeField]
+    private PlayerWeapon primaryWeapon;
+
+    [SerializeField]
+    private Transform weaponHolder;
+
+    private PlayerWeapon currentWeapon;
+
+    [SerializeField]
+    private string weaponLayerName = "Weapon";
+
+    private void Start()
+    {
+        EquipWeapon(primaryWeapon);
+    }
+
+    public PlayerWeapon GetCurrentWeapon()
+    {
+        return currentWeapon;
+    }
+
+    void EquipWeapon (PlayerWeapon _weapon)
+    {
+        currentWeapon = _weapon;
+        GameObject _weaponIns = (GameObject)Instantiate(_weapon.graphics, weaponHolder.position, weaponHolder.rotation);
+        _weaponIns.transform.SetParent(weaponHolder);
+        if (isLocalPlayer)
+            _weaponIns.layer = LayerMask.NameToLayer(weaponLayerName);
+    }
+}
+
